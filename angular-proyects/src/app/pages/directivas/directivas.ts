9c5d6a6ebd347b9684b22babf1a87f6e67a210e3
sadesuu <hsolistorrijos@gmail.com>
2026-01-12 18:04:50 +0100

    feat: add table display for alumnos with status badges and refactor directivas layout

--- angular-proyects/src/app/pages/directivas/directivas.ts
+++ angular-proyects/src/app/pages/directivas/directivas.ts
@@ -1,4 +1,6 @@
+import { FetchBackend } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-directivas',
@@ -9,19 +11,52 @@ import { Component } from '@angular/core';
 export class Directivas {
   // Ejemplo @if
   mostrarMensaje = false;
-  usuario = { nombre: 'Hugo', edad: 21 };
+  usuario = { nombre: 'Hugo', edad:  7};
   spiderman = { nombre: 'Spiderman', poder: 'telaraña' };
   batman = { nombre: 'Batman', poder: 'tecnología' };
   heroes: 'Spiderman' | 'Batman' = 'Spiderman';
   // Ejemplo @for
-  palabras = ['Casimiro', 'Apruebame', 'Porfa', 'Te lo pido', 'Hugo'];
+  alumnos:any[] = [
+  {
+    id: 1,
+    nombre: 'Hugo',
+    puesto: 'Desarrollador',
+    cantidad: 3,
+    estado: 'activo', 
+    fecha: '2024-06-15',
+  },
+  {
+    id: 2,
+    nombre: 'Ana',
+    puesto: 'Diseñadora',
+    cantidad: 5,
+    estado: 'inactivo', 
+    fecha: '2024-05-20',
+  },
+  {
+    id: 3,
+    nombre: 'Luis',
+    puesto: 'Tester',
+    cantidad: 2,
+    estado: 'activo', 
+    fecha: '2024-04-10',
+  },
+  {
+    id: 4,
+    nombre: 'María',
+    puesto: 'Project Manager',
+    cantidad: 4,
+    estado: 'pendiente', 
+    fecha: '2024-03-25',
+  },
+   ];
   frutas = ['Manzana', 'Banana', 'Naranja', 'Mango', 'Pera'];
   usuarios = [
     { id: 1, nombre: 'Ana', rol: 'Admin' },
     { id: 2, nombre: 'Luis', rol: 'Usuario' },
     { id: 3, nombre: 'María', rol: 'Editor' }
+  
   ];
-
   // Ejemplo @switch
   tipoUsuario: 'admin' | 'editor' | 'usuario' | 'invitado' = 'admin';
   estadoPedido: 'pendiente' | 'procesando' | 'enviado' | 'entregado' = 'pendiente';
